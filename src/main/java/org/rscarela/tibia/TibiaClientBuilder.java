2f6f05268f78ec61b368bc3150b789d60277da25
Renan Scarela <renan.scarela@gmail.com>
2017-07-07 12:18:16 -0300

    Creating attributes on TibiaClientBuilder to handle forbidden status due to request amount.

--- src/main/java/org/rscarela/tibia/TibiaClientBuilder.java
+++ src/main/java/org/rscarela/tibia/TibiaClientBuilder.java
@@ -10,6 +10,9 @@ public class TibiaClientBuilder {
 	private static final String DEFAULT_TIBIA_URL = "http://www.tibia.com";
 
 	private String baseURL;
+	private boolean handleForbiddenStatus;
+	private Integer forbiddenStatusDelay;
+	private Integer retryAttempts;
 
 	public final TibiaClient build() {
 		return new TibiaClient(getConnector());
@@ -25,6 +28,21 @@ public class TibiaClientBuilder {
 
 	public TibiaClientBuilder setBaseURL(String baseURL) {
 		this.baseURL = baseURL;
+		return this;
+	}
+
+	public TibiaClientBuilder setHandleForbiddenStatus(boolean handleForbiddenStatus) {
+		this.handleForbiddenStatus = handleForbiddenStatus;
+		return this;
+	}
+
+	public TibiaClientBuilder setForbiddenStatusDelay(Integer forbiddenStatusDelay) {
+		this.forbiddenStatusDelay = forbiddenStatusDelay;
+		return this;
+	}
+
+	public TibiaClientBuilder setRetryAttempts(Integer retryAttempts) {
+		this.retryAttempts = retryAttempts;
 		return this;
 	}
 
